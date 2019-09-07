80781aec31df6ac9ffcfc69c8b40da0036b19e2d
ypablo <yfedora@gmail.com>
2019-09-07 14:15:02 +0100

    Add all functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <TourList/>
       </div>
     );
 
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
+import Tour from '../Tour';
+import './tourlist.scss';
+import {TourData, tourData} from '../../tourData';
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData
+    }
+
+    removeTour = (id) => {
+       const {tours} = this.state;
+       const sortedTours = tours.filter(tour => tour.id !== id)
+       this.setState ({
+           tours: sortedTours
+       })
+    }
+
     render() {
+
+        const {tours} = this.state;
         return (
-            <div>
-                
+            <div className="tourList">
+                {
+                    tours.map(tour => (
+                        <Tour key={Tour.id} tour={tour} removeTour={this.removeTour}></Tour>
+                    ) )
+                }
             </div>
         )
     }
